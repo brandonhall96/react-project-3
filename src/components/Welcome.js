dbad401129d97d2c7d3a647c866f1213b31ea687
brandonhall96 <hall.bran96@gmail.com>
2021-06-08 19:34:32 -0400

    added tracker comp

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Welcome = () => {
     return (
         <div>
-            <h1>Welcome to the thunderdome</h1>
+            <h1>This is our Dashboard</h1>
         </div>
     )
 }
