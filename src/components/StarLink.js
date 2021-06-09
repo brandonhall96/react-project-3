078c73acd3d6c36a6bac823e447b082c3d1db2de
brandonhall96 <hall.bran96@gmail.com>
2021-06-08 21:20:35 -0400

    started starlink api

--- src/components/StarLink.js
+++ src/components/StarLink.js
@@ -3,7 +3,23 @@ import React from 'react';
 
 
 
+
+
+
 const StarLink = () => {
+
+    useEffect(() =>{
+        let url = 'https://api.spacexdata.com/v4/starlink'
+        
+        
+
+    })
+
+
+
+
+
+
     return(
         <div>
             <h1>This is starlink</h1>
