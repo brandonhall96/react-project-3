46755db83b468629e0f3f11df9f35996abb58946
vinnyvilasboa <vinnycesca@gmail.com>
2021-06-14 13:11:44 -0400

    making the styling similiar on add pages - added new font to welcome page

--- README.md
+++ README.md
@@ -1,11 +1,59 @@
-
 ![Imgur](https://i.imgur.com/yq7noDD.png)
-
-# WHAT IT IS 
-
-
-## HOW IT WORKS 
-
-
-## STRETCH GOALS
-
+![](https://i.imgur.com/ikboIVi.png)
+![Issues](https://img.shields.io/github/issues/brandonhall96/react-project-3)![Forks](https://img.shields.io/github/forks/brandonhall96/react-project-3)![License](https://img.shields.io/github/stars/brandonhall96/react-project-3)![Repo-Size](	https://img.shields.io/github/license/brandonhall96/react-project-3)![Contributors](https://img.shields.io/github/contributors/brandonhall96/Project-3-Backend)
+Fake-X-front-End is the frontend of a full space-x starlink tracker 
+## Collaborators
+ ```sh
+ https://github.com/jakeschultz89
+ https://github.com/brandonhall96
+ https://github.com/vinnyvilasboa
+ https://github.com/behemoth0132
+ ```
+- Type "npm run star-craco" in terminal
+- ✨Magic ✨
+## Features
+- Use the seed .js file to add the static data we included in order to view it in mongoDB
+- The data used in the seed will reflect to the front end. 
+- The magic is just getting started please scroll to the end to see the link for the front end!
+## Tech
+Fake-X-Front-End uses the only technology that matters to work properly:
+- [React] - React. js is an open-source JavaScript library that is used for building user interfaces specifically for single-page applications
+## Installation
+Fake-X requires [Node.js](https://nodejs.org/) to run.
+You first need to clone the repository
+```
+git clone https://github.com/jakeschultz89/react-project-3.git
+```
+Install the dependencies and devDependencies and start the server.
+```sh
+npm i
+npm start
+```
+For .env... use whatever values you would like for the items included in the .env file below
+```sh
+MONGO_URI=""
+JWT_SECRET=""
+```
+Next... Go to your terminal and 
+```sh 
+Run the command (mongod) first
+```
+```sh
+Then follow up with the (mongo) command in a seperate terminal window
+```
+## Code Snippets to be Proud of 
+![](https://i.imgur.com/ES6BDXv.png)
+> The above code snippet is something we are very proud of due to the fact that we went through alot of trial and error to get this to work. The purpose of the above function is to update a specific ```astro``` that has already been created, initially we used the ```update``` method and that didnt work because it only updated the first ```astro``` in the database instead of the one we tried to pull and update by the i.d.. Once we changed it to ```findByIDAndUpdate``` it fixed all of our problems but we defninently had to troubleshoot this for a few days but thankfully with some needed help we got it to work.
+## Schema Idea
+![](https://i.imgur.com/25wp8LI.jpg)
+> Above is our thinking behind our schema's and how they connect the User schema is self explanitory it collects the information input by the user and saves it to the database, this schema connects to the ```Astros Scehma``` meaning a user can create an astro and conduct other things such as update and delete them. The other schemas are stactic data that we seeded into our databse from the Space-X-API ```https://github.com/r-spacex/SpaceX-API/tree/master/docs/v4``` 
+Verify the deployment by navigating to your server address in
+your preferred browser.
+```sh
+http://localhost:8080
+```
+> our backend is also deployed on heroku at ```https://afternoon-lowlands-89410.herokuapp.com/```
+> In order to see the full app please view our front end ```https://github.com/brandonhall96/react-project-3```
+> you will find additional information there concerning this app.
+   [React]: <https://https://reactjs.org//>
+   [node.js]: <http://nodejs.org>
--- src/pages/StarLink.js
+++ src/pages/StarLink.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React , {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 import App from '../App.css'
 import Tracker from './Tracker';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
@@ -9,150 +9,134 @@ import setAuthToken from '../utils/setAuthToken'
 
 const StarLink = (props) => {
 
- const [name, setName] = useState('')
- const [longitude, setLongitude] = useState('')
- const [latitude, setLatitude] = useState('')
- const [launchDate, setLaunchDate] = useState('')
+    const [name, setName] = useState('')
+    const [longitude, setLongitude] = useState('')
+    const [latitude, setLatitude] = useState('')
+    const [launchDate, setLaunchDate] = useState('')
 
- const [name2, setName2] = useState('')
- const [longitude2, setLongitude2] = useState('')
- const [latitude2, setLatitude2] = useState('')
- const [launchDate2, setLaunchDate2] = useState('')
+    const [name2, setName2] = useState('')
+    const [longitude2, setLongitude2] = useState('')
+    const [latitude2, setLatitude2] = useState('')
+    const [launchDate2, setLaunchDate2] = useState('')
 
- const [name3, setName3] = useState('')
- const [longitude3, setLongitude3] = useState('')
- const [latitude3, setLatitude3] = useState('')
- const [launchDate3, setLaunchDate3] = useState('')
+    const [name3, setName3] = useState('')
+    const [longitude3, setLongitude3] = useState('')
+    const [latitude3, setLatitude3] = useState('')
+    const [launchDate3, setLaunchDate3] = useState('')
 
- const [name4, setName4] = useState('')
- const [longitude4, setLongitude4] = useState('')
- const [latitude4, setLatitude4] = useState('')
- const [launchDate4, setLaunchDate4] = useState('')
+    const [name4, setName4] = useState('')
+    const [longitude4, setLongitude4] = useState('')
+    const [latitude4, setLatitude4] = useState('')
+    const [launchDate4, setLaunchDate4] = useState('')
 
- const [name5, setName5] = useState('')
- const [longitude5, setLongitude5] = useState('')
- const [latitude5, setLatitude5] = useState('')
- const [launchDate5, setLaunchDate5] = useState('')
+    const [name5, setName5] = useState('')
+    const [longitude5, setLongitude5] = useState('')
+    const [latitude5, setLatitude5] = useState('')
+    const [launchDate5, setLaunchDate5] = useState('')
 
-//  const [satData, setSatData] = useState([]);
-
-
-
- useEffect(() => {
-     setAuthToken(localStorage.getItem("jwtToken"))
-     let url = 'https://api.spacexdata.com/v4/starlink/5eed7716096e590006985825'
-     axios.get(url).then(res => {
-        
-         setName(res.data.spaceTrack.OBJECT_NAME)
-         setLongitude(res.data.longitude)
-         setLatitude(res.data.latitude)
-         setLaunchDate(res.data.spaceTrack.LAUNCH_DATE)
-     })
- })
-
-
- useEffect(() => {
-     setAuthToken(localStorage.getItem("jwtToken"))
-     let url = 'https://api.spacexdata.com/v4/starlink/5eed7714096e590006985623'
-     axios.get(url).then(res => {
-         
-         setName2(res.data.spaceTrack.OBJECT_NAME)
-         setLongitude2(res.data.longitude)
-         setLatitude2(res.data.latitude)
-         setLaunchDate2(res.data.spaceTrack.LAUNCH_DATE)
-     })
- })
-
- useEffect(() => {
-    setAuthToken(localStorage.getItem("jwtToken"))
-     let url = 'https://api.spacexdata.com/v4/starlink/5eed7714096e590006985634'
-     axios.get(url).then(res => {
-        
-         setName3(res.data.spaceTrack.OBJECT_NAME)
-         setLongitude3(res.data.longitude)
-         setLatitude3(res.data.latitude)
-         setLaunchDate3(res.data.spaceTrack.LAUNCH_DATE)
-     })
- })
-
- useEffect(() => {
-    setAuthToken(localStorage.getItem("jwtToken"))
-     let url = 'https://api.spacexdata.com/v4/starlink/5eed7714096e59000698563f'
-     axios.get(url).then(res => {
-        
-         setName4(res.data.spaceTrack.OBJECT_NAME)
-         setLongitude4(res.data.longitude)
-         setLatitude4(res.data.latitude)
-         setLaunchDate4(res.data.spaceTrack.LAUNCH_DATE)
-     })
- })
-
- useEffect(() => {
-    setAuthToken(localStorage.getItem("jwtToken"))
-     let url = 'https://api.spacexdata.com/v4/starlink/5eed7714096e590006985648'
-     axios.get(url).then(res => {
-         
-         setName5(res.data.spaceTrack.OBJECT_NAME)
-         setLongitude5(res.data.longitude)
-         setLatitude5(res.data.latitude)
-         setLaunchDate5(res.data.spaceTrack.LAUNCH_DATE)
-     })
- })
-//  useEffect(() => {
-//      let url = 'https://api.spacexdata.com/v4/starlink/'
-//      axios.get(url).then(res => {
-//          setSatData(res.data)
-         
-//         //  console.log(res.data)
-         
-//      })
-     
-//  })
-
- 
+    //  const [satData, setSatData] = useState([]);
 
 
 
+    useEffect(() => {
+
+        let url = 'https://api.spacexdata.com/v4/starlink/5eed7716096e590006985825'
+
+        axios.get(url).then(res => {
+
+            setName(res.data.spaceTrack.OBJECT_NAME)
+            setLongitude(res.data.longitude)
+            setLatitude(res.data.latitude)
+            setLaunchDate(res.data.spaceTrack.LAUNCH_DATE)
+        })
+    })
 
 
-    return(
-        
+    useEffect(() => {
+        let url = 'https://api.spacexdata.com/v4/starlink/5eed7714096e590006985623'
+
+        axios.get(url).then(res => {
+
+            setName2(res.data.spaceTrack.OBJECT_NAME)
+            setLongitude2(res.data.longitude)
+            setLatitude2(res.data.latitude)
+            setLaunchDate2(res.data.spaceTrack.LAUNCH_DATE)
+        })
+    })
+
+    useEffect(() => {
+        let url = 'https://api.spacexdata.com/v4/starlink/5eed7714096e590006985634'
+
+        axios.get(url).then(res => {
+
+            setName3(res.data.spaceTrack.OBJECT_NAME)
+            setLongitude3(res.data.longitude)
+            setLatitude3(res.data.latitude)
+            setLaunchDate3(res.data.spaceTrack.LAUNCH_DATE)
+        })
+    })
+
+    useEffect(() => {
+        let url = 'https://api.spacexdata.com/v4/starlink/5eed7714096e59000698563f'
+
+        axios.get(url).then(res => {
+
+            setName4(res.data.spaceTrack.OBJECT_NAME)
+            setLongitude4(res.data.longitude)
+            setLatitude4(res.data.latitude)
+            setLaunchDate4(res.data.spaceTrack.LAUNCH_DATE)
+        })
+    })
+
+    useEffect(() => {
+        let url = 'https://api.spacexdata.com/v4/starlink/5eed7714096e590006985648'
+
+        axios.get(url).then(res => {
+
+            setName5(res.data.spaceTrack.OBJECT_NAME)
+            setLongitude5(res.data.longitude)
+            setLatitude5(res.data.latitude)
+            setLaunchDate5(res.data.spaceTrack.LAUNCH_DATE)
+        })
+    })
+    return (
+
         <div id="star">
             <div className="sats">
                 <h1>{name}</h1>
-                
+
                 Latitude: {latitude}
                 <br></br>
                 Longitude: {longitude}
                 <br></br>
                 Launch Date: {launchDate}
                 <br></br>
-            
-            </div> 
+
+            </div>
             <br></br>
             <div>
                 <h1>{name2}</h1>
-                
+
                 Latitude: {latitude2}
                 <br></br>
                 Longitude: {longitude2}
                 <br></br>
                 Launch Date: {launchDate2}
                 <br></br>
-               
-            </div> 
+
+            </div>
             <br></br>
             <div>
                 <h1>{name3}</h1>
-                
+
                 Latitude: {latitude3}
                 <br></br>
                 Longitude: {longitude3}
                 <br></br>
                 Launch Date: {launchDate3}
                 <br></br>
-             
-            </div> 
+
+            </div>
             <br></br>
             <div>
                 <h1>{name4}</h1>
@@ -162,9 +146,9 @@ const StarLink = (props) => {
                 <br></br>
                 Launch Date: {launchDate4}
                 <br></br>
-              
-            </div> 
-                <br></br>
+
+            </div>
+            <br></br>
             <div>
                 <h1>{name5}</h1>
                 Latitude: {latitude5}
@@ -173,27 +157,26 @@ const StarLink = (props) => {
                 <br></br>
                 Launch Date: {launchDate5}
                 <br></br>
-                
-            </div> 
 
-        
+            </div>
+
+
             {/* <Tracker /> */}
 
             <div id="aboutstar">
-            Starlink is a satellite internet constellation being constructed by FakeX providing satellite Internet access. 
-            The constellation will consist of thousands of mass-produced 
-            small satellites in low Earth orbit, which communicate with designated ground transceivers.
+                <div className="row mt-4">
+                    <div className="col-md-8 offset-md-3">
+                        <div className="card card-body">
+                            <div class="text-center">
+                                Starlink is a satellite internet constellation being constructed by FakeX providing satellite Internet access.
+                                The constellation will consist of thousands of mass-produced
+                                small satellites in low Earth orbit, which communicate with designated ground transceivers.
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            
-            <button class='starbutton'><a href='/tracker'>View in Tracker</a></button>
-                
-                
-       
-           
-          
-
-
+            <button id='starbutton' class="btn btn-danger"><a href='/tracker'>View in Tracker</a></button>
         </div>
     )
 }
